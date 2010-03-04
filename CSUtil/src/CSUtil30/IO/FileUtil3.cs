8ab28a2e95914f20640e755286ae44c2167086d4
dot-station <dot-station@localhost>
2010-03-04 09:37:56 +0900

    FileInfo????????

---
+++ CSUtil/src/CSUtil30/IO/FileUtil3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace CSUtil.IO
+{
+    /// <summary>
+    /// File操作ユーティリティ(.net 3.0以降)
+    /// </summary>
+    public static class FileUtil3
+    {
+        /// <summary>
+        /// 指定されたディレクトリ配下の全てのファイルをFileInfoとして列挙します。
+        /// </summary>
+        /// <param name="baseDir">列挙するディレクトリ</param>
+        /// <returns>FileInfoの列挙子</returns>
+        public static IEnumerable<FileInfo> EnFileInfo(string baseDir)
+        {
+            return from path in Directory.GetFiles(baseDir, "*.*", SearchOption.AllDirectories)
+                   select new FileInfo(path);
+        }
+    }
+}
