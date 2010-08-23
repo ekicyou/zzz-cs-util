9d6bd65514c4e8709b209d419eaee34ffa864607
dot-station <dot-station@localhost>
2010-08-23 09:55:59 +0900

    ??????

--- CSUtil/src/CSUtil/ComFactory.cs
+++ CSUtil/src/CSUtil/ComFactory.cs
@@ -4,34 +4,37 @@ using System.Text;
 
 namespace CSUtil
 {
-  public static class ComFactory
-  {
     /// <summary>
-    /// COMオブジェクトへの参照を作成および取得する
+    /// Com参照ユーティリティ
     /// </summary>
-    /// <param name="progId">作成するオブジェクトのプログラムID</param>
-    /// <param name="serverName">
-    /// オブジェクトが作成されるネットワーク サーバーの名前
-    /// </param>
-    /// <returns>作成されたCOMオブジェクト</returns>
-    public static object CreateObject(string progId, string serverName)
+    public static class ComFactory
     {
-      Type t;
-      if (serverName == null || serverName.Length == 0)
-        t = Type.GetTypeFromProgID(progId);
-      else
-        t = Type.GetTypeFromProgID(progId, serverName, true);
-      return Activator.CreateInstance(t);
-    }
+        /// <summary>
+        /// COMオブジェクトへの参照を作成および取得する
+        /// </summary>
+        /// <param name="progId">作成するオブジェクトのプログラムID</param>
+        /// <param name="serverName">
+        /// オブジェクトが作成されるネットワーク サーバーの名前
+        /// </param>
+        /// <returns>作成されたCOMオブジェクト</returns>
+        public static object CreateObject(string progId, string serverName)
+        {
+            Type t;
+            if (serverName == null || serverName.Length == 0)
+                t = Type.GetTypeFromProgID(progId);
+            else
+                t = Type.GetTypeFromProgID(progId, serverName, true);
+            return Activator.CreateInstance(t);
+        }
 
-    /// <summary>
-    /// COMオブジェクトへの参照を作成および取得する
-    /// </summary>
-    /// <param name="progId">作成するオブジェクトのプログラムID</param>
-    /// <returns>作成されたCOMオブジェクト</returns>
-    public static object CreateObject(string progId)
-    {
-      return CreateObject(progId, null);
+        /// <summary>
+        /// COMオブジェクトへの参照を作成および取得する
+        /// </summary>
+        /// <param name="progId">作成するオブジェクトのプログラムID</param>
+        /// <returns>作成されたCOMオブジェクト</returns>
+        public static object CreateObject(string progId)
+        {
+            return CreateObject(progId, null);
+        }
     }
-  }
 }
